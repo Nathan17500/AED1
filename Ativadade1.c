5251ba7624667a6be62db357187417d94352acb4
Nathanael Martins <nathanael.sa@alunos.ufersa.edu.br>
2026-08-19 22:58:52 -0300

    alterando Atividade 1

--- Ativadade1.c
+++ Ativadade1.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
-void testetesouro (int *ptesouro) {
-    if (*ptesouro==0) {
-        printf ("TESOURO: INATIVO\n");
-    } else {
-        printf ("TESOURO: ATIVO\n");
+
+void testetesouro(int *ptesouro){
+    if (*ptesouro == 0){
+        printf("TESOURO: INATIVO\n\n");
+    }else{
+        printf("TESOURO: ATIVO\n\n");
     }
 }
-void vidaatual (int *pvida) {
-    printf ("VIDA: %d\n", *pvida);
-}
-int main () {
-    int vida=100;
-    int tesouro=0;
-    int *pvida = &vida;
-    int *ptesouro = &tesouro;
 
-    vidaatual (pvida);
-    testetesouro (ptesouro);
-    *pvida=*pvida-40;
-    
+void verificaVidaAtual(int *pvida){
+    printf("VIDA: %d\n", *pvida);
+}
+int main() {
+    int vidaInicial = 100, vidaAtual = vidaInicial, tesouroInicial = 0, tesouroAtual = tesouroInicial;
+    int *pvida = &vidaAtual, *ptesouro = &tesouroAtual;
+
+    printf("Estado Inicial\n");
+    verificaVidaAtual(pvida);
+    testetesouro(ptesouro);
+    printf("Enquanto se aventurava em busca de tesouro, você cai em um buraco. Você recebe 20 de dano.\n");
+    *pvida = *pvida - 30;
+    verificaVidaAtual(pvida);
+    printf("Lá dentro, você se depara com um local ideal para descansar. Você recupera sua vida!\n");
+    *pvida = *pvida + 20;
+    verificaVidaAtual(pvida);
+    printf("Após descansar, você adentra mais a caverna, encontrando o tesouro que tanto procurava e encerrando o dia.\n\n");
+    *ptesouro=1;
+    printf("Início do dia\nVIDA: %d\n", vidaInicial);
+    if (tesouroInicial==0) {
+        printf("TESOURO: INATIVO\n");
+    } else {
+        printf("TESOURO: ATIVO\n");
+    }
+    printf("\nFim do dia\n");
+    verificaVidaAtual(pvida);
+    testetesouro(ptesouro);
+
     return 0;
 }
