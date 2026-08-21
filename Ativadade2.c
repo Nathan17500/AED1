2fcf27a37c90b0f4610df1b15bf7a4375646319e
Nathanael Martins <nathanael.sa@alunos.ufersa.edu.br>
2026-08-21 17:20:07 -0300

    Finalizando Atividade 2

--- Ativadade2.c
+++ Ativadade2.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
-void aplicarDano(int *pvida, int dano) {
+void aplicar_pontuação_dupla (int *ppontuação) {
+    *ppontuação=*ppontuação * 2;
+}
+
+void aplicar_dano(int *pvida, int dano) {
     *pvida=*pvida - dano;
 }
 
-void aplicarCura(int *pvida, int cura) {
+void restaurar_vida(int *pvida, int cura) {
     *pvida=*pvida + cura;
 }
 
-void testetesouro(int *ptesouro){
+void testeTesouro(int *ptesouro){
     if (*ptesouro == 0){
-        printf("TESOURO: INATIVO\n%p\n\n", (void*) ptesouro);
+        printf("TESOURO: INATIVO\n%p\n", (void*) ptesouro);
     }else{
-        printf("TESOURO: ATIVO\n%p\n\n", (void*) ptesouro);
+        printf("TESOURO: ATIVO\n%p\n", (void*) ptesouro);
     }
 }
 
@@ -20,30 +24,45 @@ void verificaVidaAtual(int *pvida){
     printf("VIDA: %d\n%p\n", *pvida, (void*) pvida);
 }
 
-int main() {
-    int vidaInicial = 100, vidaAtual = vidaInicial, tesouroInicial = 0, tesouroAtual = tesouroInicial;
-    int *pvida = &vidaAtual, *ptesouro = &tesouroAtual;
+void testePontuação (int *ppontuação){
+    printf("PONTUAÇÃO: %d\n%p\n\n", *ppontuação, (void*) ppontuação);
+}
 
-    printf("Estado Inicial\n");
-    verificaVidaAtual(pvida);
-    testetesouro(ptesouro);
-    printf("Enquanto se aventurava em busca de tesouro, você cai em um buraco. Você recebe 30 de dano.\n");
-    aplicarDano(pvida, 30);
-    verificaVidaAtual(pvida);
-    printf("Lá dentro, você se depara com um local ideal para descansar. Você recupera sua vida!\n");
-    aplicarCura(pvida, 20);
-    verificaVidaAtual(pvida);
-    printf("Após descansar, você adentra mais a caverna, encontrando o tesouro que tanto procurava e encerrando o dia.\n\n");
-    *ptesouro=1;
-    printf("Início do dia\nVIDA: %d\n", vidaInicial);
-    if (tesouroInicial==0) {
-        printf("TESOURO: INATIVO\n");
-    } else {
-        printf("TESOURO: ATIVO\n");
-    }
-    printf("\nFim do dia\n");
-    verificaVidaAtual(pvida);
-    testetesouro(ptesouro);
+int main() {
+    int vidaInicial = 100, vidaAtual = vidaInicial, tesouroInicial = 0, tesouroAtual = tesouroInicial, pontuaçãoInicial = 0, pontuaçãoAtual = pontuaçãoInicial;
+    int *pvida = &vidaAtual, *ptesouro = &tesouroAtual, *ppontuação = &pontuaçãoAtual;
+
+        printf("Estado Inicial\n");
+        verificaVidaAtual(pvida);
+        testeTesouro(ptesouro);
+        testePontuação(ppontuação);
+
+        printf("Enquanto se aventurava em busca de tesouro, você cai em um buraco. Você recebe 30 de dano.\n");
+        aplicar_dano(pvida, 30);
+        verificaVidaAtual(pvida);
+
+        printf("Lá dentro, você se depara com um local ideal para descansar. Você recupera sua vida e ganha 100 pontos!\n");
+        restaurar_vida(pvida, 20);
+        *ppontuação = *ppontuação + 100;
+        verificaVidaAtual(pvida);
+        testePontuação(ppontuação);
+
+        printf("Após descansar, você adentra mais a caverna, encontrando o tesouro que tanto procurava, duplicando seus pontos e encerrando o dia.\n\n");
+        *ptesouro=1;
+        aplicar_pontuação_dupla(ppontuação);
+
+        printf("Início do dia\nVIDA: %d\n", vidaInicial);
+        if (tesouroInicial==0) {
+            printf("TESOURO: INATIVO\n");
+        } else {
+            printf("TESOURO: ATIVO\n");
+        }
+        printf("PONTUAÇÃO: %d\n", pontuaçãoInicial);
+
+        printf("\nFim do dia\n");
+        verificaVidaAtual(pvida);
+        testeTesouro(ptesouro);
+        testePontuação(ppontuação);
 
     return 0;
 }
