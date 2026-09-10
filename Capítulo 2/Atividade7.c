2232cd60deb1a1fb31cd3f6ed6518c0640893693
Nathanael Martins <nathanael.sa@alunos.ufersa.edu.br>
2026-09-10 09:06:51 -0300

    Adicionando Atividade 7.

--- Capítulo 2/Atividade7.c
+++ Capítulo 2/Atividade7.c
@@ -2,8 +2,8 @@
 #include <string.h>
 
 int main () {
-    char *nome_jogadores[3], *senha_jogadores[3];
-    char nome[30], senha[30];
+    char *nome_jogadores[3], *senha_jogadores[3], *apelido_jogadores[3];
+    char nome[30], senha[30], senha_confirmação[30], apelido[30];
 
     for (int i=0; i<3; i++) {
         printf("Informe o nome do jogador %d: \n", i+1);
@@ -11,15 +11,38 @@ int main () {
         nome[strcspn(nome, "\n")] = '\0';
         if (strlen(nome) == 0) {
             do {
-            printf ("Nome inválido. Informe novamente:\n");
-            fgets(nome,sizeof(nome), stdin);
+                printf ("Nome inválido. Informe novamente:\n");
+                fgets(nome,sizeof(nome), stdin);
             } while (strlen(nome) == 0);
         }
         nome_jogadores[i] = nome;
-        printf("Informe a senha do jogador %d: \n");
+
+        printf("Informe o apelido do jogador %d: \n", i+1);
+        fgets(apelido, sizeof(apelido), stdin);
+        apelido[strcspn(apelido, "\n")] = '\0';
+        apelido_jogadores[i] = apelido;
+
+        printf("Informe a senha do jogador %d: \n", i+1);
         fgets(senha, sizeof(senha), stdin);
-        senha[strcspn(nome, "\n")] = '\0';
-
+        senha[strcspn(senha, "\n")] = '\0';
+        if (strlen(senha) == 0) {
+            do {
+                printf ("Senha inválida. Informe novamente:\n");
+                fgets(senha, sizeof(senha), stdin);
+            } while (strlen(senha) == 0);
+        }
+        printf("Confirme sua senha:\n");
+        fgets(senha_confirmação, sizeof(senha_confirmação), stdin);
+        senha_confirmação[strcspn(senha_confirmação, "\n")] = '\0';
+        if (strcmp (senha, senha_confirmação) != 0) {
+            while (strcmp (senha, senha_confirmação) != 0) {
+                printf("Senha errada. Informe novamente.\n");
+                fgets(senha_confirmação,sizeof(senha_confirmação), stdin);
+                senha_confirmação[strcspn(senha_confirmação, "\n")] = '\0';
+            }
+            senha_jogadores[i] = senha;
+        } else {
+            senha_jogadores[i] = senha;
+        }
     }
-
 }
